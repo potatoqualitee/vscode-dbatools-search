8ee84d7c81e92c0f32fdd7b6b53bd87c7e9926a8
Chrissy LeMaire <clemaire@gmail.com>
2018-11-25 12:54:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# dbatools search for vs code
+
+<img align="left" src=https://blog.netnerds.net/wp-content/uploads/2016/05/dbatools.png alt="dbatools logo"> Perform a web search on dbatools.io and docs.dbatools.io of selected text using a handy context menu entry.
+
+## Features
+
+This dbatools extension allows you to search dbatools.io and docs.dbatools.io for the highlighted text. The results are displayed in the default browser.
+
+## Configuration
+
+If you run into any problems, please log an issue on [GitHub](https://github.com/potatoqualitee/vscode-dbatoolsdocs/issues).
+
+
+![dbatools search](resources/search.gif)
---
+++ extension.js
@@ -0,0 +1,52 @@
+
+// Constant strings 
+const CFG_SECTION = "dbatoolsSearch";
+const CFG_QUERY = "QueryTemplate";
+const CMD_ID = "extension.dbatoolsSearch";
+
+var vscode = require('vscode');
+
+function activate(context) {
+
+  var disposable = vscode.commands.registerTextEditorCommand(CMD_ID, WebSearch);
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
+
+// Launches the search URL in default browser
+function WebSearch() {
+  var selectedText = GetSelectedText();
+  if (!selectedText)
+    return;
+
+  var uriText = encodeURI(selectedText);
+
+  var dbatoolsSearchCfg = vscode.workspace.getConfiguration(CFG_SECTION);
+  const queryTemplate = dbatoolsSearchCfg.get(CFG_QUERY);
+  var query = queryTemplate.replace("%SELECTION%", uriText);
+
+  vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(query));
+}
+
+// Creates the search URL based on the selection and the query template.
+function GetSelectedText() {
+  const documentText = vscode.window.activeTextEditor.document.getText();
+  if (!documentText)
+    return '';
+
+  var activeSelection = vscode.window.activeTextEditor.selection;
+  if (activeSelection.isEmpty)
+    return '';
+
+  const selStartOffset = vscode.window.activeTextEditor.document.offsetAt(activeSelection.start);
+  const selEndOffset = vscode.window.activeTextEditor.document.offsetAt(activeSelection.end);
+
+  var selectedText = documentText.slice(selStartOffset, selEndOffset).trim();
+  selectedText = selectedText.replace(/\s\s+/g, ' ');
+  return selectedText;
+}
