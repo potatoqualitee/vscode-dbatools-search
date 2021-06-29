aca911be06b27f31933b8143cf784a78a936ebbd
Chrissy LeMaire <clemaire@gmail.com>
2021-06-29 12:17:13 +0200

    add highlight selection - fixes #8

--- src/utils/getSelectedText.ts
+++ src/utils/getSelectedText.ts
@@ -3,28 +3,31 @@ import * as vscode from 'vscode';
 
 export function getSelectedText(): string {
 	const activeEditor = vscode.window.activeTextEditor;
-    if (!activeEditor) {
-		console.debug("no active editor");
-        return '';
-    }
-
-	const documentText = activeEditor!.document.getText();
-	if (!documentText) {
-	  console.debug("bailed on getText");
-	  return '';
-	}
 
 	var activeSelection = activeEditor!.selection;
 	if (activeSelection.isEmpty) {
-	  console.debug("bailed on selection");
-	  return '';
+		let cursorPosition = activeEditor!.selection.start;
+		let wordRange = activeEditor!.document.getWordRangeAtPosition(cursorPosition);
+		let highlight = activeEditor!.document.getText(wordRange);
+		return highlight;
 	}
-  
+
+	if (!activeEditor) {
+		console.debug("no active editor");
+		return '';
+	}
+
+	const documentText = activeEditor!.document.getText();
+	if (!documentText) {
+		console.debug("bailed on getText");
+		return '';
+	}
+
 	const selStartOffset = activeEditor!.document.offsetAt(activeSelection.start);
 	const selEndOffset = activeEditor!.document.offsetAt(activeSelection.end);
-  
+
 	var selectedText = documentText.slice(selStartOffset, selEndOffset).trim();
 	selectedText = selectedText.replace(/\s\s+/g, ' ');
 	console.debug(`selected text is ${selectedText}`);
 	return selectedText;
-  }
+}
