c3e82f754baec36fee9091af10ab01742085097c
Chrissy LeMaire <clemaire@gmail.com>
2024-11-02 09:03:34 +0100

    upd

--- dist/extension.js
+++ dist/extension.js
@@ -1,171 +1,129 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ([
-/* 0 */,
-/* 1 */
-/***/ ((module) => {
-
-module.exports = require("vscode");
-
-/***/ }),
-/* 2 */
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
 };
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-__exportStar(__webpack_require__(3), exports);
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
+// src/extension.ts
+var extension_exports = {};
+__export(extension_exports, {
+  activate: () => activate,
+  deactivate: () => deactivate
+});
+module.exports = __toCommonJS(extension_exports);
+var vscode3 = __toESM(require("vscode"));
 
-/***/ }),
-/* 3 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+// src/utils/searcher.ts
+var vscode2 = __toESM(require("vscode"));
 
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.searcher = searcher;
-const vscode = __webpack_require__(1);
-const getSelectedText_1 = __webpack_require__(4);
-function searcher(template) {
-    var selectedText = (0, getSelectedText_1.getSelectedText)();
-    if (template === "VSCodeWorkbench") {
-        vscode.commands.executeCommand('workbench.action.findInFiles', {
-            query: selectedText,
-            triggerSearch: true
-        });
-    }
-    else {
-        if (!selectedText) {
-            return;
-        }
-        var uriText = encodeURI(selectedText);
-        var search = vscode.workspace.getConfiguration("search");
-        const qt = `queryTemplate.${template}`;
-        const queryTemplate = search.get(`queryTemplate.${template}`);
-        var query = queryTemplate.replace("-searchphrase-", uriText);
-        console.debug(`query is ${queryTemplate}`);
-        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(query));
-    }
-}
-
-
-/***/ }),
-/* 4 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.getSelectedText = getSelectedText;
-const vscode = __webpack_require__(1);
+// src/utils/getSelectedText.ts
+var vscode = __toESM(require("vscode"));
 function getSelectedText() {
-    const activeEditor = vscode.window.activeTextEditor;
-    const activeSelection = activeEditor.selection;
-    if (activeSelection.isEmpty) {
-        let cursorPosition = activeEditor.selection.start;
-        let wordRange = activeEditor.document.getWordRangeAtPosition(cursorPosition);
-        let highlight = activeEditor.document.getText(wordRange);
-        console.debug(`no explicitly selected text. highlighted on ${highlight}`);
-        return highlight;
-    }
-    const documentText = activeEditor.document.getText();
-    if (!documentText) {
-        console.debug("bailed on getText");
-        return '';
-    }
-    const selStartOffset = activeEditor.document.offsetAt(activeSelection.start);
-    const selEndOffset = activeEditor.document.offsetAt(activeSelection.end);
-    var selectedText = documentText.slice(selStartOffset, selEndOffset).trim();
-    selectedText = selectedText.replace(/\s\s+/g, ' ');
-    console.debug(`selected text is ${selectedText}`);
-    return selectedText;
+  const activeEditor = vscode.window.activeTextEditor;
+  const activeSelection = activeEditor.selection;
+  if (activeSelection.isEmpty) {
+    let cursorPosition = activeEditor.selection.start;
+    let wordRange = activeEditor.document.getWordRangeAtPosition(cursorPosition);
+    let highlight = activeEditor.document.getText(wordRange);
+    console.debug(`no explicitly selected text. highlighted on ${highlight}`);
+    return highlight;
+  }
+  const documentText = activeEditor.document.getText();
+  if (!documentText) {
+    console.debug("bailed on getText");
+    return "";
+  }
+  const selStartOffset = activeEditor.document.offsetAt(activeSelection.start);
+  const selEndOffset = activeEditor.document.offsetAt(activeSelection.end);
+  var selectedText = documentText.slice(selStartOffset, selEndOffset).trim();
+  selectedText = selectedText.replace(/\s\s+/g, " ");
+  console.debug(`selected text is ${selectedText}`);
+  return selectedText;
 }
 
+// src/utils/searcher.ts
+function searcher(template) {
+  var selectedText = getSelectedText();
+  if (template === "VSCodeWorkbench") {
+    vscode2.commands.executeCommand("workbench.action.findInFiles", {
+      query: selectedText,
+      triggerSearch: true
+    });
+  } else {
+    if (!selectedText) {
+      return;
+    }
+    var uriText = encodeURI(selectedText);
+    var search = vscode2.workspace.getConfiguration("search");
+    const qt = `queryTemplate.${template}`;
+    const queryTemplate = search.get(`queryTemplate.${template}`);
+    var query = queryTemplate.replace("-searchphrase-", uriText);
+    console.debug(`query is ${queryTemplate}`);
+    vscode2.commands.executeCommand("vscode.open", vscode2.Uri.parse(query));
+  }
+}
 
-/***/ })
-/******/ 	]);
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
-(() => {
-var exports = __webpack_exports__;
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.activate = activate;
-exports.deactivate = deactivate;
-const vscode = __webpack_require__(1);
-const utils_1 = __webpack_require__(2);
+// src/extension.ts
 function activate(context) {
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.docsSearch", () => {
-        (0, utils_1.searcher)("Docs");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.googleSearch", () => {
-        (0, utils_1.searcher)("Google");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.dbatoolsSearch", () => {
-        (0, utils_1.searcher)("dbatools");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.stackSearch", () => {
-        (0, utils_1.searcher)("Stackoverflow");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.duckSearch", () => {
-        (0, utils_1.searcher)("Duckduckgo");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.thwackSearch", () => {
-        (0, utils_1.searcher)("Thwack");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.technetSearch", () => {
-        (0, utils_1.searcher)("Technet");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.vscodeApiSearch", () => {
-        (0, utils_1.searcher)("VSCodeAPI");
-    }));
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.vscodeWorkspaceSearch", () => {
-        (0, utils_1.searcher)("VSCodeWorkbench");
-    }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.docsSearch", () => {
+    searcher("Docs");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.googleSearch", () => {
+    searcher("Google");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.dbatoolsSearch", () => {
+    searcher("dbatools");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.stackSearch", () => {
+    searcher("Stackoverflow");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.duckSearch", () => {
+    searcher("Duckduckgo");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.thwackSearch", () => {
+    searcher("Thwack");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.technetSearch", () => {
+    searcher("Technet");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.vscodeApiSearch", () => {
+    searcher("VSCodeAPI");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.vscodeWorkspaceSearch", () => {
+    searcher("VSCodeWorkbench");
+  }));
+  context.subscriptions.push(vscode3.commands.registerTextEditorCommand("extension.chatGptSearch", () => {
+    searcher("ChatGPT");
+  }));
 }
-function deactivate() { }
-
-})();
-
-module.exports = __webpack_exports__;
-/******/ })()
-;
+function deactivate() {
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  activate,
+  deactivate
+});
 //# sourceMappingURL=extension.js.map
