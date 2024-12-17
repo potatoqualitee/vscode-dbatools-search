2d8b08cb4bbdd91a8a66455efd8148c107f3d119
Chrissy LeMaire <clemaire@gmail.com>
2024-12-17 13:11:42 +0100

    Add GitHub

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,18 @@ import * as vscode from 'vscode';
 import { searcher } from './utils';
 
 export function activate(context: vscode.ExtensionContext) {
+	// GitHub searches first
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.githubSearch", () => {
+		searcher("GitHub");
+	}));
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.githubRepoSearch", () => {
+		searcher("GitHubRepo");
+	}));
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.githubAuthorSearch", () => {
+		searcher("GitHubAuthor");
+	}));
+
+	// Other searches
 	context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.docsSearch", () => {
 		searcher("Docs");
 	}));
